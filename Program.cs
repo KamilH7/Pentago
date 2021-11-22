f4355c7dc30eeab805568a120c4bdf36940ed0a8
Kamil Haraf <kamilharaf7@gmail.com>
2021-11-22 20:40:27 +0100

    Add AlphaBeta Pruning

--- Program.cs
+++ Program.cs
@@ -20,21 +20,22 @@ namespace PentagoMinMax
             */
 
 
-            RandomBot player1 = new RandomBot(Player.Player1);
+            NegaMax player1 = new NegaMax(Player.Player1, 5);
             //MiniMaxBot player1 = new MiniMaxBot(Player.Player1, 2);
-            MiniMaxBot player2 = new MiniMaxBot(Player.Player2, 1);
+            AlpaBeta player2 = new AlpaBeta(Player.Player2, 5);
+            //RandomBot player2 = new RandomBot(Player.Player2);
 
-            const double numOfSimulations = 1000;
+            const double numOfSimulations = 1;
             double numWonByPlayer1 = 0;
             double numWonByPlayer2 = 0;
             double numofDraws = 0;
-
+            double player1MoveTime = 0;
+            double player2MoveTime = 0;
 
             for (int i = 0; i < numOfSimulations; i++)
-            {
-                //Console.Clear();
+            {      
                 Console.WriteLine("Simulating... " + Math.Round((numWonByPlayer1 + numWonByPlayer2 + numofDraws) / numOfSimulations * 100, 2) + "% complete");
-                PentagoSimulation simulation = new PentagoSimulation(new Pentago(), player1, player2, true);
+                PentagoSimulation simulation = new PentagoSimulation(new Pentago(), player1, player2);
 
                 WinType outcome = simulation.StartSimulation();
 
@@ -44,11 +45,23 @@ namespace PentagoMinMax
                     numWonByPlayer1 += 1;
                 if (outcome == WinType.Draw)
                     numofDraws += 1;
-            }
 
-            Console.WriteLine("% won by Player1(" + player1.playerType.ToString() + "):" + 100 * numWonByPlayer1 / numOfSimulations); ;
+                player1MoveTime += simulation.player1AvgMoveTime;
+                player2MoveTime += simulation.player2AvgMoveTime;
+            }
+            Console.Clear();
+
+            Console.WriteLine("% won by Player1(" + player1.playerType.ToString() + "):" + 100 * numWonByPlayer1 / numOfSimulations);
             Console.WriteLine("% won by Player2(" + player2.playerType.ToString() + "):" + 100 * numWonByPlayer2 / numOfSimulations);
             Console.WriteLine("% drawed:" + 100 * numofDraws / numOfSimulations);
+
+            Console.WriteLine();
+
+            player1MoveTime = Math.Round(player1MoveTime, 2);
+            player2MoveTime = Math.Round(player2MoveTime, 2);
+
+            Console.WriteLine("Avg movetime of Player1(" + player1.playerType.ToString() + "):" + player1MoveTime / numOfSimulations + "ms");
+            Console.WriteLine("Avg movetime of Player2(" + player2.playerType.ToString() + "):" + player2MoveTime / numOfSimulations + "ms");
             Console.ReadLine();
 
         }
